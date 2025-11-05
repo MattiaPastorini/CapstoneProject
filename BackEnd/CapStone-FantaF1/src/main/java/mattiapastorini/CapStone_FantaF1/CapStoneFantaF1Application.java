1d76bf880c167814cb7dfa4d032a672c5b24b648
MattiaPastorini <mattia.862002@gmail.com>
2025-11-05 09:34:34 +0100

    Aggiunto backend Spring Boot al progetto

---
+++ BackEnd/CapStone-FantaF1/src/main/java/mattiapastorini/CapStone_FantaF1/CapStoneFantaF1Application.java
@@ -0,0 +1,13 @@
+package mattiapastorini.CapStone_FantaF1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapStoneFantaF1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapStoneFantaF1Application.class, args);
+	}
+
+}
---
+++ BackEnd/CapStone-FantaF1/src/test/java/mattiapastorini/CapStone_FantaF1/CapStoneFantaF1ApplicationTests.java
@@ -0,0 +1,13 @@
+package mattiapastorini.CapStone_FantaF1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapStoneFantaF1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
